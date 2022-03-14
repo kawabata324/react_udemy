1247d520ce601c3eff50479a95a36d987464f77d
kawabata <s1931115@s.konan-u.ac.jp>
2022-03-14 18:34:07 +0900

    complete todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,81 @@
-import {ColorfulMessage} from "./components/ColorfulMessage";
-import {useEffect, useState} from "react";
+import './styles.css'
+import {useState} from "react";
 
-const App = () => {
-    const [num, setNum] = useState(0);
-    const [faceShowFlag, setFaceShowFlag] = useState(false)
+export const App = () => {
+    const [todoText, setTodoText] = useState('')
+    const [incompleteTodos, setIncompleteTodos] = useState([])
+    const [completeTodos, setCompleteTodos] = useState([])
 
-    const onClickCountUp = () => {
-        setNum(num + 1)
+    const onChangeTodoText = (event) => setTodoText(event.target.value)
+
+    const onClickAdd = () => {
+        if (todoText === "") return
+        const newTodos = [...incompleteTodos, todoText];
+        setIncompleteTodos(newTodos);
+        setTodoText('')
     }
-    const onClickSwitchShowFlag = () => {
-        setFaceShowFlag(!faceShowFlag)
+
+    const onClickDelete = (index) => {
+        const newTodos = [...incompleteTodos]
+        newTodos.splice(index, 1)
+        setIncompleteTodos(newTodos)
     }
 
-    useEffect(() => {
-        if (num % 3 === 0 && num > 0) {
-            faceShowFlag || setFaceShowFlag(true)
-        } else {
-            faceShowFlag && setFaceShowFlag(false)
-        }
-    }, [num])
-    return (<>
-        <h1 style={{color: 'red'}}>こんにちは</h1>
-        <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
-        <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-        <button onClick={onClickCountUp}>カウントアップ</button>
-        <br/>
-        <button onClick={onClickSwitchShowFlag}>on/off</button>
-        <p>{num}</p>
-        {faceShowFlag && <p>٩( ᐛ )و</p>}
-    </>)
+    const onClickComplete = (index) => {
+        const newCompleteTodo = incompleteTodos[index]
+        const newCompleteTodos = [...completeTodos, newCompleteTodo]
+
+        const newIncompleteTodos = [...incompleteTodos]
+        newIncompleteTodos.splice(index, 1)
+
+        setCompleteTodos(newCompleteTodos)
+        setIncompleteTodos(newIncompleteTodos)
+    }
+
+    const onClickReturn = (index) => {
+        const newIncompleteTodo = completeTodos[index]
+        const newIncompleteTodos = [...incompleteTodos, newIncompleteTodo]
+
+        const newCompleteTodos = [...completeTodos]
+        newCompleteTodos.splice(index, 1)
+
+        setIncompleteTodos(newIncompleteTodos)
+        setCompleteTodos(newCompleteTodos)
+    }
+
+    return (
+        <>
+            <div className="input-area">
+                <input type="text" placeholder="Todoを入力" value={todoText} onChange={onChangeTodoText}/>
+                <button onClick={onClickAdd}>追加</button>
+            </div>
+            <div className="incomplete-area">
+                <p className="title">未完了のTodo</p>
+                <ul>
+                    {incompleteTodos.map((todo, index) => {
+                        return (
+                            <div key={todo} className="list-row">
+                                <li>{todo}</li>
+                                <button onClick={() => onClickComplete(index)}>完了</button>
+                                <button onClick={() => onClickDelete(index)}>削除</button>
+                            </div>
+                        )
+                    })}
+                </ul>
+            </div>
+            <div className="complete-area">
+                <p className="title">完了のTodo</p>
+                <ul>
+                    {completeTodos.map((todo, index) => {
+                        return (
+                            <div key={todo} className="list-row">
+                                <li>{todo}</li>
+                                <button onClick={() => onClickReturn(index)}>戻す</button>
+                            </div>
+                        )
+                    })}
+                </ul>
+            </div>
+        </>
+    )
 };
-
-export default App;
